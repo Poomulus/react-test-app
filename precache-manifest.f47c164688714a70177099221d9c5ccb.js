b404430916e6c56e87b26f7247cf6155bb470770
Paul Daly <paulie5457@gmail.com>
2020-09-13 17:20:58 -0700

    Updates

---
+++ precache-manifest.f47c164688714a70177099221d9c5ccb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9599735ad3d1a5b3f9bb045a85b17b64",
+    "url": "/react-test-app/index.html"
+  },
+  {
+    "revision": "ca5c4d4c09f5d380d0c5",
+    "url": "/react-test-app/static/css/main.203bb415.chunk.css"
+  },
+  {
+    "revision": "320eea99ce92e09d1c0d",
+    "url": "/react-test-app/static/js/2.dfd0624c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test-app/static/js/2.dfd0624c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca5c4d4c09f5d380d0c5",
+    "url": "/react-test-app/static/js/main.988fcd11.chunk.js"
+  },
+  {
+    "revision": "b9b9d3b8d5765dbfd572",
+    "url": "/react-test-app/static/js/runtime-main.03f37cb0.js"
+  }
+]);
